680fa925747709881bc190394b5c8fe62c70d50b
ossve <38524037+ossve@users.noreply.github.com>
2018-04-19 10:58:49 +0200

    asd
    asd

--- Assign2/subplots_task6.m
+++ Assign2/subplots_task6.m
@@ -3,29 +3,33 @@ hold all
 
 ax1 = subplot(2,2,1);
 x = t/w_base;
-y1 = sqrt(i_s_s_alpha.^2+i_s_s_beta.^2);
-plot(ax1,x,y1)
-title(ax1,'Stator Current - Direct Line Start, b=0')
+y11 = sqrt(i_s_s_alpha.^2+i_s_s_beta.^2);
+plot(ax1,x,y1,x,y11)
+legend('Direct Line Start', 'V/f Control');
+title(ax1,'Stator Current - Direct Line Start and V/f Control, b=0')
 ylabel(ax1,'Stator Current Modulus [p.u]')
 xlabel(ax1,'Time [p.u]');
 
 ax1 = subplot(2,2,2);
-y1 = sqrt(psi_r_s_alpha.^2+psi_r_s_beta.^2);
-plot(ax1,x,y1)
-title(ax1,'Rotor Flux - Direct Line Start, b=0')
+y22 = sqrt(psi_r_s_alpha.^2+psi_r_s_beta.^2);
+plot(ax1,x,y2,x,y22)
+legend('Direct Line Start', 'V/f Control');
+title(ax1,'Rotor Flux - Direct Line Start and V/f Control, b=0')
 ylabel(ax1,'Rotor Flux Modulus [p.u]')
 xlabel(ax1,'Time [p.u]');
 
 ax1 = subplot(2,2,3);
-y1 = Te;
-plot(ax1,x,y1)
-title(ax1,'Torque - Direct Line Start, b=0')
+y33 = Te;
+plot(ax1,x,y3,x,y33)
+legend('Direct Line Start', 'V/f Control');
+title(ax1,'Torque - Direct Line Start and V/f Control, b=0')
 ylabel(ax1,'Torque [p.u]')
 xlabel(ax1,'Time [p.u]');
 
 ax1 = subplot(2,2,4);
-y1 = wr;
-plot(ax1,x,y1)
-title(ax1,'Speed - Direct Line Start, b=0')
+y44 = wr;
+plot(ax1,x,y4,x,y44)
+legend('Direct Line Start', 'V/f Control');
+title(ax1,'Speed - Direct Line Start and V/f Control, b=0')
 ylabel(ax1,'Speed [p.u]')
 xlabel(ax1,'Time [p.u]');
